d3afa4d875f28c653836b11305428fe3532c7b0b
Daniel Martinez <roanhawkeye@gmail.com>
2017-03-24 16:55:42 -0600

    Add functionality to show image button

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent {
 	pageTitle: string = 'Product List';
 	imageWidth: number = 50;
 	imageMargin: number = 2;
+	showImage: boolean = false;
 	products: any[] = [
 		{
         "productId": 1,
@@ -29,5 +30,9 @@ export class ProductListComponent {
 	        "starRating": 4.2,
 	        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
 	    }
-	]; 
+	];
+
+	toggleImage():void {
+		this.showImage = !this.showImage;
+	} 
 }
