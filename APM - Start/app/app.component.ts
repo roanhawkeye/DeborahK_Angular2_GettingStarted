504af20704642b2b8770953336493758c4456bfc
Daniel Martinez <roanhawkeye@gmail.com>
2017-03-28 12:57:00 -0600

    Register and use product service

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component({
     selector: 'pm-app',
     moduleId: module.id,
-    templateUrl: 'app.component.html' 
+    templateUrl: 'app.component.html',
+    providers: [ ProductService ] 
 })
 export class AppComponent { 
 	pageTitle: string = 'Acme Product Management';
