84500099a2cf2818ca9f0f09b81f5454c0d99062
Daniel Martinez <roanhawkeye@gmail.com>
2017-03-31 15:08:54 -0600

    Implement missing details for detail product template

--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     moduleId: module.id,
@@ -10,13 +11,18 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit{
     pageTitle: string = "Product Detail";
     product: IProduct;
+    errorMessage: string;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router){}
+                private _router: Router,
+                private _productService: ProductService){}
 
     ngOnInit():void {
         let id = +this._route.snapshot.params['id'];
         this.pageTitle += `: ${id}`;
+        this._productService.getProduct(id)
+            .subscribe(product => this.product = product, 
+                        error => this.errorMessage = <any>error);
     }
 
     onBack(): void{
